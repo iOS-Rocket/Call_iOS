83d7a2a384244eb4a45bbe09e3cbefe0fb598c55
Juhui <joohee090331@gmail.com>
2025-09-12 22:55:39 +0900

    setting :: 프로젝트 세팅

---
+++ Call_iOS/Call_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Call_iOS
+//
+//  Created by 신주희 on 9/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
